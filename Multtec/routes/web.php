d9ad6a554dc581f76262c552239f15dfdd059a9d
Pedro <marqeess99@gmail.com>
2018-01-13 13:38:53 -0200

    Estruturação dos arquivos

--- Multtec/routes/web.php
+++ Multtec/routes/web.php
@@ -1,6 +1,28 @@
 <?php
 
 
-Route::get('/', function () {
-    return view('welcome');
+// Rota inicial ---------------------------------------------------------------
+
+Route::get('/' , 'SiteController@inicio')->name('siteinicio');
+
+// Rota de contato ------------------------------------------------------------
+
+Route::get('contato' , 'SiteController@contato')->name('sitecontato');
+
+// Rota sobre -----------------------------------------------------------------
+
+Route::get('sobre' , 'SiteController@sobre')->name('sitesobre');
+
+
+// Rota de administração do site ----------------------------------------------
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::prefix('admin')->group(function () {
+
+        Route::resource('contato', 'ContatoController');
+        Route::resource('servicos', 'ServicoController');
+
+    });
+
 });
